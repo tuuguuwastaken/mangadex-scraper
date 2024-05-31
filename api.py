708ab6eb04b11224dbb34b765f5a7aa1c9967266
tuguldur jon <tuguldur.j@ailab.mn>
2024-05-31 17:44:55 +0800

    upate

--- api.py
+++ api.py
@@ -4,61 +4,72 @@ import json
 import os
 
 class Downloader:
-    def __init__(self,url=None,output="./images") -> None:
+    def __init__(self, url=None, output="./images") -> None:
         self.output = output
         self.url = url
         self.baseURLmanga = "https://api.mangadex.org/chapter?limit=100&manga={}"
         self.baseURLChapter = "https://api.mangadex.org/at-home/server/{}"
 
-    def extractId(self,url):
-        pattern = r'([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})'
+    def extractId(self, url):
+        pattern = r"([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
         uuid = re.search(pattern, url).group(1)
-        print(uuid) 
+        print(uuid)
         return uuid
-    
-    def getChapterData(self,id):
+
+    def getChapterData(self, id):
         res = requests.get(self.baseURLmanga.format(id))
-        data = res.json().get('data')
+        data = res.json().get("data")
+        print(json.dumps(data, indent=4))
         ids = []
         chapCount = 0
+        seen_chapters = set()  # Set to track seen chapters
         try:
             for chapter in data:
-                if chapter.get("attributes").get("translatedLanguage") == "en":
-                    ids.append({"id":chapter.get('id'),"name":f"Chapter {chapter.get('attributes').get('chapter') or chapCount}"})
-                    chapCount = chapCount + 1
-                    for id in ids:
-                        chapterResponse = requests.get(self.baseURLChapter.format(id.get("id")))
-                        baseURL = chapterResponse.json().get("baseUrl")
-                        hash = chapterResponse.json().get("chapter").get("hash")
-                        imgs = chapterResponse.json().get("chapter").get("data")
-                        
-                        imgUrl = "{}/data/{}/{}"
-                        count = 1
-                        for img in imgs:
-                            url = imgUrl.format(baseURL, hash, img)
-                            imgRes = requests.get(url)
-                            img_data = imgRes.content
-                            if not imgRes.status_code == 200:
-                                print("Failed to download image. Status code:", imgRes.status_code)
-                                return 0
-                            output = os.path.join(self.output,id.get("name"))
-                            if not os.path.exists(output):
-                                os.makedirs(output)
-                            image_filename = os.path.join(output, f"{count}.jpg")
-                            with open(image_filename, "wb") as image_file:
-                                print(f"Downloaded {id.get('name')} : {count}/ {len(imgs)}")
-                                image_file.write(img_data)
-                            count = count + 1
+                chapter_num = chapter.get("attributes").get("chapter")
+                if chapter.get("attributes").get("translatedLanguage") == "en" and chapter_num not in seen_chapters:
+                    seen_chapters.add(chapter_num)  # Mark this chapter as seen
+                    ids.append(
+                        {
+                            "id": chapter.get("id"),
+                            "name": f"Chapter {chapter_num or chapCount}",
+                        }
+                    )
+                    chapCount += 1
+            for id in ids:
+                chapterResponse = requests.get(
+                    self.baseURLChapter.format(id.get("id"))
+                )
+                baseURL = chapterResponse.json().get("baseUrl")
+                hash = chapterResponse.json().get("chapter").get("hash")
+                imgs = chapterResponse.json().get("chapter").get("data")
+
+                imgUrl = "{}/data/{}/{}"
+                count = 1
+                for img in imgs:
+                    url = imgUrl.format(baseURL, hash, img)
+                    imgRes = requests.get(url)
+                    img_data = imgRes.content
+                    if imgRes.status_code != 200:
+                        print(
+                            "Failed to download image. Status code:",
+                            imgRes.status_code,
+                        )
+                        return 0
+                    output = os.path.join(self.output, id.get("name"))
+                    if not os.path.exists(output):
+                        os.makedirs(output)
+                    image_filename = os.path.join(output, f"{count}.jpg")
+                    with open(image_filename, "wb") as image_file:
+                        print(
+                            f"Downloaded {id.get('name')} : {count}/ {len(imgs)}"
+                        )
+                        image_file.write(img_data)
+                    count += 1
         except Exception as e:
             return e
-        
-        finally:
-            return True
+        return True
 
-
-    def start(self,url=None):
-        # https://mangadex.org/title/960a03a6-0c21-470c-be84-973304b2f7bd/oh-my-devil
-        # https://api.mangadex.org/chapter?limit=100&manga={id}
+    def start(self, url=None):
         if not url:
             url = self.url
         if not url and not self.url:
@@ -69,8 +80,8 @@ class Downloader:
         self.getChapterData(mangaId)
 
 
-
-
 if __name__ == "__main__":
-    api = Downloader(url="https://mangadex.org/title/ff4669dd-c512-4d30-aeb7-6ceec5308a76/saihate-ni-madou",)
+    api = Downloader(
+        url="https://mangadex.org/title/adce0aea-3f6d-4fd2-9902-1107f59f94da/arafoo-kenja-no-isekai-seikatsu-nikki",
+    )
     api.start()
--- converter.py
+++ converter.py
@@ -1,29 +1,39 @@
 from PIL import Image
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 class Converter:
-    def __init__(self, input="./images", output="./webps") -> None:
-        self.input = input
-        self.output = output
-        
+    def __init__(self, input_dir="./images", output_dir="./webps") -> None:
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+
+    def convert_image(self, img_info):
+        file, image = img_info
+        img_path = os.path.join(self.input_dir, file, image)
+        output_path = os.path.join(self.output_dir, file, os.path.splitext(image)[0] + '.webp')
+        with Image.open(img_path) as img:
+            img.save(output_path, 'WEBP')
+        print(f"Converted {file}/{image}")
 
     def start(self):
-        if not os.path.exists(self.output):
-            os.makedirs(self.output)
-        files = os.listdir(self.input)
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        
+        files = os.listdir(self.input_dir)
+        tasks = []
+
         for file in files:
-            images = os.listdir(os.path.join(self.input, file))
-            if not os.path.exists(os.path.join(self.output, file)):
-                os.makedirs(os.path.join(self.output, str(file)))
-            count = 1
+            images = os.listdir(os.path.join(self.input_dir, file))
+            if not os.path.exists(os.path.join(self.output_dir, file)):
+                os.makedirs(os.path.join(self.output_dir, file))
+
             for image in images:
                 if image.lower().endswith('.png') or image.lower().endswith('.jpg'):
-                    img_path = os.path.join(self.input, file, image)
-                    output_path = os.path.join(self.output, file, os.path.splitext(image)[0] + '.webp')
-                    with Image.open(img_path) as img:
-                        print(f"converted {file} : {count} / {len(images)}")
-                        img.save(output_path, 'WEBP')
-                        count = count + 1
+                    tasks.append((file, image))
+
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.convert_image, tasks)
+
         return True
 
 if __name__ == "__main__":
